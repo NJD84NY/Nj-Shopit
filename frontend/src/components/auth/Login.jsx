311bc9dab08dd732c385f0490de61320b7cdb7bd
nejad <nynjd1984@gmail,com>
2025-08-03 00:00:41 +0330

    version issue

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
 import { useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
